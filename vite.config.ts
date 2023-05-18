ca09a1fd2b2c38e747aa594d5fde515c5eeb5fb5
emerson asto <emerar.mct@gmail.com>
2023-05-18 07:51:56 -0500

    reduced data size

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,25 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dsv from '@rollup/plugin-dsv';
 
+function differMuiSourcemapsPlugins() {
+  const muiPackages = ['@mui/material', '@emotion/styled', '@emotion/react'];
+
+  return {
+    name: 'differ-mui-sourcemap',
+    transform(code: string, id: string) {
+      if (muiPackages.some(pkg => id.includes(pkg))) {
+        return {
+          code: code,
+          map: null,
+        };
+      }
+    },
+  };
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dsv()],
+  plugins: [react(), dsv(), differMuiSourcemapsPlugins()],
   server: {
     port: 3000,
   },
