94d9a414486059bac0ae4a98528e2e3e76d8ca24
emerson asto <emerar.mct@gmail.com>
2023-09-26 01:23:36 -0500

    [fix] fixed date problems

--- src/services/weatherServices.ts
+++ src/services/weatherServices.ts
@@ -1,12 +1,12 @@
 import envVars from "../configs";
 import { IWeather } from "../types/locationstypes";
-import dayjs, { convertIsoToDate } from '../utils/datesUtils';
+import dayjs from '../utils/datesUtils';
 
 export const getWeatherDataService = async (location_id: number | undefined, date: string | null) => {
   if (!location_id || !date) {
     throw new Error("location_id is required");
   }
-  const dateObj = dayjs(convertIsoToDate(date), 'DD/MM/YYYY');
+  const dateObj = dayjs(date, 'DD-MM-YYYY');
 
   const since = dateObj.startOf("day").format('YYYY-MM-DDTHH:mm:ss.SSSZ')
   const to = dateObj.endOf("day").format('YYYY-MM-DDTHH:mm:ss.SSSZ');
