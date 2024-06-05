bbb09ced2639202ca0025c61e7e77b27034671de
eastor112 <emerar.mct@gmail.com>
2024-06-05 11:28:54 -0500

    [fix] fixed problem with material ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@emotion/react';
+import { ThemeProvider } from '@mui/material';
 import { CssBaseline } from '@mui/material';
 import theme from './theme';
 import './index.css';
