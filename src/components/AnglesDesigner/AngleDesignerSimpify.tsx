72d38bcd6e512975350e1285d6fdada0db16d882
eastor112 <emerar.mct@gmail.com>
2024-06-23 18:07:58 -0500

    [change] refactor AngleDesignerSimplify

--- src/components/AnglesDesigner/AngleDesignerSimpify.tsx
+++ src/components/AnglesDesigner/AngleDesignerSimpify.tsx
@@ -1,58 +1,89 @@
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import { generalStyles } from '../../styles/general/index';
 import { useDispatch } from 'react-redux';
 import { useAppSelector } from '../../hooks/reduxHooks';
 import { setAzimuth, setInclination } from '../../redux/designerSlice';
-import TextField from '@mui/material/TextField';
-import { FocusEvent } from 'react';
+import InputField from '../InputField/InputField';
+
+const validationSchema = yup.object({
+  inclination: yup
+    .number()
+    .required('Inclinación es requerida')
+    .min(0, 'La inclinación debe ser mayor o igual a 0')
+    .max(90, 'La inclinación debe ser menor o igual a 90'),
+  azimuth: yup
+    .number()
+    .required('Azimuth es requerido')
+    .min(0, 'El azimuth debe ser mayor o igual a 0')
+    .max(360, 'El azimuth debe ser menor o igual a 360'),
+});
 
 const AnglesDesignerSimplify = () => {
   const dispatch = useDispatch();
   const { azimuth, inclination } = useAppSelector(state => state.designer);
 
-  const selectOnFocus = (
-    event: FocusEvent<HTMLInputElement | HTMLTextAreaElement, Element>
-  ) => {
-    event.target.select();
-  };
+  const formik = useFormik({
+    initialValues: {
+      inclination,
+      azimuth,
+    },
+    validationSchema: validationSchema,
+    onSubmit: values => {
+      dispatch(setInclination(Number(values.inclination)));
+      dispatch(setAzimuth(Number(values.azimuth)));
+    },
+  });
 
   return (
     <Box>
       <Typography variant='h3' sx={generalStyles.h3}>
         Ángulos
       </Typography>
-      <Box
-        sx={{ width: '100%', display: 'flex', flexDirection: 'column', gap: 2 }}
-      >
-        <TextField
-          type='number'
-          size='small'
-          id='inclination'
-          label='Inclinación'
-          variant='outlined'
-          name='inclination'
-          value={inclination}
-          onChange={val => {
-            dispatch(setInclination(+val.target.value));
+      <form onSubmit={formik.handleSubmit}>
+        <Box
+          sx={{
+            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
           }}
-          onFocus={selectOnFocus}
-        />
+        >
+          <InputField
+            id='inclination'
+            name='inclination'
+            label='Inclinación'
+            type='number'
+            value={formik.values.inclination}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={
+              formik.touched.inclination && Boolean(formik.errors.inclination)
+            }
+            helperText={formik.touched.inclination && formik.errors.inclination}
+            tooltipText='Ángulo de inclinación del panel solar (0-90 grados)'
+            sx={{ width: '100%' }}
+            size='small'
+          />
 
-        <TextField
-          type='number'
-          size='small'
-          id='azimuth'
-          label='Azimuth'
-          variant='outlined'
-          name='azimuth'
-          value={azimuth}
-          onChange={val => {
-            dispatch(setAzimuth(+val.target.value));
-          }}
-          onFocus={selectOnFocus}
-        />
-      </Box>
+          <InputField
+            id='azimuth'
+            name='azimuth'
+            label='Azimuth'
+            type='number'
+            value={formik.values.azimuth}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.touched.azimuth && Boolean(formik.errors.azimuth)}
+            helperText={formik.touched.azimuth && formik.errors.azimuth}
+            tooltipText='Ángulo de azimuth del panel solar (0-360 grados)'
+            sx={{ width: '100%' }}
+            size='small'
+          />
+        </Box>
+      </form>
     </Box>
   );
 };
