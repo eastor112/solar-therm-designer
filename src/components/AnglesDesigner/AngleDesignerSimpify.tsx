ca3c246f1dc4f5ed5410e90e7212d67a545e1967
eastor112 <emerar.mct@gmail.com>
2024-06-23 18:37:11 -0500

    [fix] fixed layout

--- src/components/AnglesDesigner/AngleDesignerSimpify.tsx
+++ src/components/AnglesDesigner/AngleDesignerSimpify.tsx
@@ -6,18 +6,7 @@ import * as yup from 'yup';
 import { useDispatch } from 'react-redux';
 import { setAzimuth, setInclination } from '../../redux/designerSlice';
 import InputField from '../InputField/InputField';
-
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 300,
-  bgcolor: 'background.paper',
-  borderRadius: '5px',
-  boxShadow: 24,
-  p: 4,
-};
+import { generalStyles } from '../../styles/general';
 
 const fieldInfo = {
   inclination: {
@@ -62,41 +51,38 @@ const AnglesDesignerSimplify: React.FC = () => {
   });
 
   return (
-    <Box sx={style}>
-      <Typography
-        id='modal-modal-title'
-        variant='h6'
-        component='h2'
-        sx={{ mb: 2 }}
-      >
+    <Box>
+      <Typography variant='h3' sx={generalStyles.h3}>
         Ángulos
       </Typography>
-      <form onSubmit={formik.handleSubmit}>
-        {Object.entries(fieldInfo).map(([field, info]) => (
-          <InputField
-            key={field}
-            id={field}
-            name={field}
-            label={info.label}
-            type='number'
-            value={formik.values[field as keyof typeof formik.values]}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            error={
-              formik.touched[field as keyof typeof formik.touched] &&
-              Boolean(formik.errors[field as keyof typeof formik.errors])
-            }
-            helperText={
-              formik.touched[field as keyof typeof formik.touched] &&
-              formik.errors[field as keyof typeof formik.errors]
-            }
-            tooltipText={info.tooltip}
-            sx={{ width: '100%', mb: 2 }}
-            size='small'
-            margin='normal'
-          />
-        ))}
-      </form>
+      <Box sx={{ width: '200px' }}>
+        <form onSubmit={formik.handleSubmit}>
+          {Object.entries(fieldInfo).map(([field, info]) => (
+            <InputField
+              key={field}
+              id={field}
+              name={field}
+              label={info.label}
+              type='number'
+              value={formik.values[field as keyof typeof formik.values]}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              error={
+                formik.touched[field as keyof typeof formik.touched] &&
+                Boolean(formik.errors[field as keyof typeof formik.errors])
+              }
+              helperText={
+                formik.touched[field as keyof typeof formik.touched] &&
+                formik.errors[field as keyof typeof formik.errors]
+              }
+              tooltipText={info.tooltip}
+              sx={{ width: '100%', mb: 2 }}
+              size='small'
+              margin='normal'
+            />
+          ))}
+        </form>
+      </Box>
     </Box>
   );
 };
