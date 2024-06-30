dca392974064beea44baeeff3f4999f52fc4af72
eastor112 <emerar.mct@gmail.com>
2024-06-30 17:12:34 -0500

    [fix] fixed coords

--- src/components/PlaceSelector/helper.ts
+++ src/components/PlaceSelector/helper.ts
@@ -2,29 +2,29 @@ export const defaultPlaces = [
   {
     id: 1,
     place: "Piura",
-    lat: -5.20000,
-    lng: -80.63000,
+    lat: -5.19449,
+    lng: -80.63282,
     altitude: 55
   },
   {
     id: 2,
     place: "Trujillo",
-    lat: -8.12000,
-    lng: -79.03000,
+    lat: -8.11599,
+    lng: -79.02998,
     altitude: 34
   },
   {
     id: 3,
     place: "Cajamarca",
-    lat: -7.16000,
-    lng: -78.51000,
+    lat: -7.16378,
+    lng: -78.50027,
     altitude: 2750
   },
   {
     id: 4,
     place: "Lima",
-    lat: -12.06000,
-    lng: -77.03000,
+    lat: -12.04318,
+    lng: -77.02824,
     altitude: 1
   }
 ];
