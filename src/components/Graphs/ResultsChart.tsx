880d4e745c502f6fd3a95a389d4a03e36c52c125
eastor112 <emerar.mct@gmail.com>
2024-07-14 10:17:20 -0500

    [fix] fixed x labels

--- src/components/Graphs/ResultsChart.tsx
+++ src/components/Graphs/ResultsChart.tsx
@@ -8,24 +8,29 @@ import {
   Legend,
   ResponsiveContainer,
   Brush,
+  XAxis,
 } from 'recharts';
 import { Box, Typography } from '@mui/material';
 import { SolarDataKeys } from '../../services/projectsServices';
 
-// const formatXLabel = (tickItem: any) => {
-//   const date = new Date(tickItem);
-//   return date.getHours() + ':' + date.getMinutes();
-// };
+function convertDecimalToTime(decimalTime: number) {
+  const hours = Math.floor(decimalTime);
+  const minutes = Math.round((decimalTime - hours) * 60);
 
-// const CustomizedXAxisTick = (props: any) => {
-//   const { x, y, payload } = props;
+  const formattedHours = hours.toString().padStart(2, '0');
+  const formattedMinutes = minutes.toString().padStart(2, '0');
 
-//   return (
-//     <text x={x} y={y + 10} textAnchor='middle' fill='#666' fontSize={12}>
-//       {formatXLabel(payload.value)}
-//     </text>
-//   );
-// };
+  return `${formattedHours}:${formattedMinutes}`;
+}
+
+const CustomizedXAxisTick = (props: any) => {
+  const { x, y, payload } = props;
+  return (
+    <text x={x} y={y + 10} textAnchor='middle' fill='#666' fontSize={12}>
+      {convertDecimalToTime(payload.value)}
+    </text>
+  );
+};
 
 const colors = ['#ce2929', '#231acc', '#27a22b', '#000', '#238787'];
 
@@ -55,32 +60,6 @@ const getheightSize = (value: string) => {
   }
 };
 
-// const CustomDateTooltip = ({ active, payload, label, units }: any) => {
-//   if (active && payload && payload.length) {
-//     const date = dayjs(label).format('DD/MM/YYYY - HH:mm');
-//     return (
-//       <Box
-//         sx={{
-//           p: 2,
-//           bgcolor: 'rgba(255,255,255,0.9)',
-//           border: '1px solid #ccc',
-//         }}
-//       >
-//         <Typography sx={{ textAlign: 'center', fontWeight: 'bold' }}>
-//           {date} H
-//         </Typography>
-//         {payload.map((item: any, index: number) => (
-//           <Typography key={index} sx={{ textAlign: 'left', color: item.color }}>
-//             {item.name}: {item.value.toFixed(2)} {units}
-//           </Typography>
-//         ))}
-//       </Box>
-//     );
-//   }
-
-//   return null;
-// };
-
 const CustomNumberArrayTooltip = ({ active, payload, units, dataKey }: any) => {
   if (active && payload && payload.length) {
     return (
@@ -97,7 +76,7 @@ const CustomNumberArrayTooltip = ({ active, payload, units, dataKey }: any) => {
               <Typography
                 sx={{ textAlign: 'center', fontWeight: 'bold', color: 'black' }}
               >
-                {item.payload[dataKey].toFixed(2)} H
+                {convertDecimalToTime(item.payload[dataKey])} H
               </Typography>
             )}
             {item.name}:
@@ -191,11 +170,11 @@ const ResultsChart: React.FC<CustomLineChartProps> = ({
           }}
         >
           <CartesianGrid strokeDasharray='2 2' />
-          {/* <XAxis
+          <XAxis
             dataKey={dataKey}
-            tick={dataKey === 'date' ? <CustomizedXAxisTick /> : undefined}
-            interval={interval}
-          /> */}
+            tick={<CustomizedXAxisTick />}
+            interval={'preserveStartEnd'}
+          />
           <YAxis domain={domain} />
           <Tooltip
             content={
